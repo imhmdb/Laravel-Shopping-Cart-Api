9a209372d7eeb3bac261a8ef1e7690db46ac1b59
M <mohamad.bahamdain@gmail.com>
2019-02-21 20:20:44 +0300

    Changes: api routes

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,18 @@
 <?php
 use Illuminate\Http\Request;
-use App\Http\Controllers\ProductController;
-
 
 Route::group(['prefix' => 'auth'], function () {
 
     Route::post('login', 'AuthController@login')->name('login');
     Route::post('signup', 'AuthController@signup');
-  
-    Route::group(['middleware' => 'auth:api'], function() {
+
+    Route::group(['middleware' => 'auth:api'], function () {
         Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
     });
-    
+
 });
 
-Route::apiResource('products', 'ProductController');
-Route::apiResource('carts', 'CartController');
-Route::post('/carts/{cart}', 'CartController@addProducts')->name('Add Products');
+Route::apiResource('products', 'ProductController')->except(['update', 'store', 'destroy']);
+Route::apiResource('carts', 'CartController')->except(['update', 'index']);
+Route::post('/carts/{cart}', 'CartController@addProducts');
+Route::post('/carts/{cart}/checkout', 'CartController@checkout');
